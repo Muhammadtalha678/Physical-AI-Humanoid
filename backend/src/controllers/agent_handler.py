36a59c992d70281ca33b2ee87f325992496a2b25
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-12-16 00:22:13 +0500

    .

--- backend/src/controllers/agent_handler.py
+++ backend/src/controllers/agent_handler.py
@@ -3,7 +3,7 @@ from src.lib.configs import settings
 from src.lib.vectordb_connection import get_qdrant
 from src.lib.google_embedding import embed_text
 from src.lib.agent_instructions import instructions
-from agents import Agent, Runner, RunConfig, FunctionTool, function_tool, RunContextWrapper
+from agents import Agent, Runner, RunConfig,  function_tool, RunContextWrapper,ModelSettings
 from dataclasses import dataclass
 import re
 from qdrant_client.http import models
@@ -16,33 +16,27 @@ class QdrantContext:
 
 @function_tool()
 async def run_query_with_context(ctx: RunContextWrapper[QdrantContext], query: str):
-    print(query)
     try:
+        print("tool called")
+        print("client",ctx.context.client)
         client = ctx.context.client
-        # print(client)
-        embedding = embed_text("Waht is Physical AI")
-        # print("embedding",embedding)
-
+        embedding = embed_text(query)
+        
         search_result = client.query_points(
             collection_name=settings.qdrant_collection_name,
             query=embedding,
             limit=5,
           
         )
-        # print(search_result)
+        # print("result_lists",search_result)
+        result_lists = []
+        # if search_result:
+            # print("result_lists",search_result[0].score)
+            # [result_lists.append(r.payload.get("chunk_text")) for r in search_result]
         return search_result
-        return {
-            "results": [
-                {
-                    "score": r.score,
-                    "file_path": r.payload.get("file_path"),
-                    "chunk_text": r.payload.get("chunk_text")
-                }
-                for r in search_result
-            ]
-        }
-
+      
     except Exception as e:
+        print("e",e)
         raise HTTPException(status_code=500, detail=str(e))
 
 
@@ -52,6 +46,10 @@ async def handle_query_operation(agent_config, query, qdrant_client=None):
         runConfig = RunConfig(
             model=agent_config.model(),
             model_provider=agent_config.client(),
+            model_settings= ModelSettings(
+                tool_choice="required",
+                
+            )
         )
 
         # Create Qdrant context with the client from app state
@@ -61,7 +59,8 @@ async def handle_query_operation(agent_config, query, qdrant_client=None):
         starting_agent = Agent(
             name="RAG Query Agent",
             instructions= instructions,
-            tools=[run_query_with_context]
+            tools=[run_query_with_context],
+            # tool_use_behavior="stop_on_first_tool"
         )
 
         # Run the agent with the user's query and context
@@ -70,8 +69,9 @@ async def handle_query_operation(agent_config, query, qdrant_client=None):
             input=query,
             run_config=runConfig,
             context=qdrant_context
+            
         )
-
+        # print("Final Output:", result.final_output)
         return {"status": "success", "query": query, "result": result.final_output}
 
     except Exception as e:
