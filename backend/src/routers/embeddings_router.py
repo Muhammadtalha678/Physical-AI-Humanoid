9775d079b3497093336a5ff50da4e2bfbd87820b
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-12-15 11:07:27 +0500

    debug

--- backend/src/routers/embeddings_router.py
+++ backend/src/routers/embeddings_router.py
@@ -3,11 +3,11 @@ from src.controllers.embeddings_controller import process_documents
 import os
 router = APIRouter()
 
-print("Current Working Directory (CWD):", os.getcwd())
-print("Files in CWD:", os.listdir())
 
 @router.post("/embeddings")
 async def create_embeddings(data: dict, request: Request):
+    print("Current Working Directory (CWD):", os.getcwd())
+    print("Files in CWD:", os.listdir())
     folder_path = data["folder_path"]
     print(folder_path)
     qdrant_client = request.app.state.qdrant_client
