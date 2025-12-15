21775f32eec7200aaf21312a9f888ee04b7b8c7d
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-12-15 10:48:23 +0500

    debug

--- backend/src/routers/embeddings_router.py
+++ backend/src/routers/embeddings_router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Request
 from src.controllers.embeddings_controller import process_documents
-
+import os
 router = APIRouter()
 
+print("Current Working Directory (CWD):", os.getcwd())
+print("Files in CWD:", os.listdir())
 
 @router.post("/embeddings")
 async def create_embeddings(data: dict, request: Request):
